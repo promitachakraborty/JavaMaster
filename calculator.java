9bef29cdcf6686e820b309911fa236be2c6792ea
Promita Chakraborty <priyapromita@gmail.com>
2021-12-23 02:17:02 +0530

    Calulations of Numbers

--- calculator.java
+++ calculator.java
@@ -1,5 +1,71 @@
-class calculator{
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+import java.util.*;
+
+public class calculator {
+  public static void main(String[] args) {
+
+    char operator;
+    Double a, b, result;
+
+   
+    Scanner input = new Scanner(System.in);
+
+   
+    System.out.println("Choose an operator: +, -, *, / or %");
+    operator = input.next().charAt(0);
+
+   
+    System.out.println("Enter first number");
+    a = input.nextDouble();
+
+    System.out.println("Enter second number");
+    b = input.nextDouble();
+
+    switch (operator) {
+
+      //Addition
+
+      case '+':
+        result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+       break;
+
+         
+      // Subtraction
+
+      case '-':
+        result = a - b;
+        System.out.println(a + " - " + b + " = " + result);
+
+           break;
+       
+      //  Multiplication
+
+      case '*':
+        result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+          
+             break;
+
+      //  Division 
+      case '/':
+        result = a / b;
+        System.out.println(a + " / " + b + " = " + result);
+
+
+            break;
+
+       //Modulo or remainder
+      case '%':
+        result = a % b;
+        System.out.println(a + " % " + b + " = " + result);
+
+             break;
+       
+
+     
+        
     }
+
+    
+  }
 }
