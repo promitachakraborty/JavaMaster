ebde2fff2576615e1f535fe176905a415a5e0c00
Promita Chakraborty <priyapromita@gmail.com>
2021-12-26 23:30:26 +0530

    Take An Integer And Reverse It

---
+++ ReverseInteger.java
@@ -0,0 +1,15 @@
+public class ReverseInteger
+
+{  
+public static void main(String[] args)   
+{  
+int number = 765432, reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
